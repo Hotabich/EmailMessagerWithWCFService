6d77d280118f6f0de28816788c74880c67e5b208
Denis <hotabich91@mail.ru>
2017-06-25 17:16:15 +0300

    add credential view

---
+++ Web_Mail/Controllers/CredentialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Mail.Controllers
+{
+    public class CredentialController : Controller
+    {
+        // GET: Credential
+        public ActionResult Credential()
+        {
+            return View();
+        }
+    }
+}
