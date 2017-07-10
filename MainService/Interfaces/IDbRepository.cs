cb2a7718824918719f44241a45ba35f7760ce4dd
Denis <hotabich91@mail.ru>
2017-07-10 16:49:27 +0300

    add DBInterfaces to server

---
+++ MainService/Interfaces/IDbRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainService.Model.Interfaces
+{
+    interface IDbRepository
+    {
+        List<string> GetAllRecipiantsList();
+        List<string> GetRecipientsList(string name);
+        void DeleteRecipiantsList(string name);
+
+        void AddRecipiant(int id, string nameList, string email);
+        void UpdateRecipiant(int id, string nameList);
+        void DeleteRecipiant(int id);
+
+
+        
+
+
+    }
+}
---
+++ MainService/Model/DBRepository/DbRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainService.Model.Interfaces;
+
+namespace MainService.Model.DBRepository
+{
+    public class DbRepository : IDbRepository
+    {
+        public void AddRecipiant(int id, string nameList, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRecipiant(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRecipiantsList(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetAllRecipiantsList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetRecipientsList(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRecipiant(int id, string nameList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
